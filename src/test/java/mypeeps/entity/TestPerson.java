3b942b911c565e5c7e45fdbf2224d72d0ce764d5
Pete Hering <pjhering@gmail.com>
2017-03-18 07:32:17 -0400

    removed unnecessary letters

--- src/test/java/mypeeps/entity/TestPerson.java
+++ src/test/java/mypeeps/entity/TestPerson.java
@@ -11,56 +11,56 @@ public class TestPerson
         
         long id = 1;
         
-        Person p0 = new Person(id++, "Gen 0", "Foo 0", "", "");
-        Person p1 = new Person(id++, "Gen 0", "Foo 1", "", "");
+        Person p0 = new Person(id++, "0", "0", "", "");
+        Person p1 = new Person(id++, "0", "1", "", "");
         
-        Person p2 = new Person(id++, "Gen 1", "Foo 2", "", "");
-        Person p3 = new Person(id++, "Gen 1", "Foo 3", "", "");
-        Person p4 = new Person(id++, "Gen 1", "Foo 4", "", "");
-        Person p5 = new Person(id++, "Gen 1", "Foo 5", "", "");
+        Person p2 = new Person(id++, "1", "2", "", "");
+        Person p3 = new Person(id++, "1", "3", "", "");
+        Person p4 = new Person(id++, "1", "4", "", "");
+        Person p5 = new Person(id++, "1", "5", "", "");
         relate(p0, p2, p3, p4, p5);
         relate(p1, p2, p3, p4, p5);
         
-        Person p6 = new Person(id++, "Gen 0", "Foo 6", "", "");
-        Person p7 = new Person(id++, "Gen 0", "Foo 7", "", "");
+        Person p6 = new Person(id++, "0", "6", "", "");
+        Person p7 = new Person(id++, "0", "7", "", "");
         
-        Person p8 = new Person(id++, "Gen 1", "Foo 8", "", "");
-        Person p9 = new Person(id++, "Gen 1", "Foo 9", "", "");
-        Person p10 = new Person(id++, "Gen 1", "Foo", "", "");
-        Person p11 = new Person(id++, "Gen 1", "Foo", "", "");
+        Person p8 = new Person(id++, "1", "8", "", "");
+        Person p9 = new Person(id++, "1", "9", "", "");
+        Person p10 = new Person(id++, "1", "10", "", "");
+        Person p11 = new Person(id++, "1", "11", "", "");
         relate(p6, p8, p9, p10, p11);
         relate(p7, p8, p9, p10, p11);
         
-        Person p12 = new Person(id++, "Gen 2", "Foo", "", "");
-        Person p13 = new Person(id++, "Gen 2", "Foo", "", "");
-        Person p14 = new Person(id++, "Gen 2", "Foo", "", "");
-        Person p15 = new Person(id++, "Gen 2", "Foo", "", "");
+        Person p12 = new Person(id++, "2", "12", "", "");
+        Person p13 = new Person(id++, "2", "13", "", "");
+        Person p14 = new Person(id++, "2", "14", "", "");
+        Person p15 = new Person(id++, "2", "15", "", "");
         relate(p2, p12, p13, p14, p15);
         relate(p8, p12, p13, p14, p15);
         
-        Person p16 = new Person(id++, "Gen 2", "Foo", "", "");
-        Person p17 = new Person(id++, "Gen 2", "Foo", "", "");
-        Person p18 = new Person(id++, "Gen 2", "Foo", "", "");
-        Person p19 = new Person(id++, "Gen 2", "Foo", "", "");
+        Person p16 = new Person(id++, "2", "16", "", "");
+        Person p17 = new Person(id++, "2", "17", "", "");
+        Person p18 = new Person(id++, "2", "18", "", "");
+        Person p19 = new Person(id++, "2", "19", "", "");
         relate(p3, p16, p17, p18, p19);
         relate(p9, p16, p17, p18, p19);
         
-        Person p20 = new Person(id++, "Gen 3", "Foo", "", "");
-        Person p21 = new Person(id++, "Gen 3", "Foo", "", "");
-        Person p22 = new Person(id++, "Gen 3", "Foo", "", "");
-        Person p23 = new Person(id++, "Gen 3", "Foo", "", "");
+        Person p20 = new Person(id++, "3", "20", "", "");
+        Person p21 = new Person(id++, "3", "21", "", "");
+        Person p22 = new Person(id++, "3", "22", "", "");
+        Person p23 = new Person(id++, "3", "23", "", "");
         relate(p12, p20, p21, p22, p23);
         relate(p16, p20, p21, p22, p23);
         
-        Person p24 = new Person(id++, "Gen 3", "Foo", "", "");
-        Person p25 = new Person(id++, "Gen 3", "Foo", "", "");
-        Person p26 = new Person(id++, "Gen 3", "Foo", "", "");
-        Person p27 = new Person(id++, "Gen 3", "Foo", "", "");
+        Person p24 = new Person(id++, "3", "24", "", "");
+        Person p25 = new Person(id++, "3", "25", "", "");
+        Person p26 = new Person(id++, "3", "26", "", "");
+        Person p27 = new Person(id++, "3", "27", "", "");
         relate(p13, p24, p25, p26, p27);
         relate(p17, p24, p25, p26, p27);
         
-        Person p28 = new Person(id++, "Gen 4", "Foo", "", "");
-        Person p29 = new Person(id++, "Gen 4", "Foo", "", "");
+        Person p28 = new Person(id++, "4", "28", "", "");
+        Person p29 = new Person(id++, "4", "29", "", "");
         relate(p20, p28, p29);
         relate(p24, p28, p29);
         
